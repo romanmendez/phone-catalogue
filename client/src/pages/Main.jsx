ceef72e7397a4cb80215a943a0bbb898c6b56281
Román Méndez <mendezbeck@gmail.com>
2020-03-25 13:06:29 +0100

    conflict merge

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -5,7 +5,10 @@ import { getPhones } from "../redux/actions";
 import DetailCard from "../components/DetailCard";
 import PhoneImage from "../components/PhoneImage";
 import { MainContainer, Row, Column, List } from "../styles/Styles";
+<<<<<<< Updated upstream
 import Loading from "../components/Loading";
+=======
+>>>>>>> Stashed changes
 
 const Main = ({ loading, phones, getPhones }) => {
   const [selected, setSelected] = useState(null);
@@ -13,6 +16,7 @@ const Main = ({ loading, phones, getPhones }) => {
   useEffect(() => {
     getPhones();
   }, []);
+<<<<<<< Updated upstream
   if (loading) {
     return <Loading />;
   } else {
@@ -28,6 +32,20 @@ const Main = ({ loading, phones, getPhones }) => {
       </MainContainer>
     );
   }
+=======
+
+  return (
+    <MainContainer>
+      <Row>
+        <Column size="25%">
+          <List>{phones && phones.map((phone, i) => <ListItem key={i} {...{ phone, setSelected }} />)}</List>
+        </Column>
+        <Column size="25%">{selected && <PhoneImage {...{ selected }} />}</Column>
+        <Column size="50%">{selected && <DetailCard {...{ selected }} />}</Column>
+      </Row>
+    </MainContainer>
+  );
+>>>>>>> Stashed changes
 };
 
 const mapStateToProps = state => ({ phones: state.phones, loading: state.loading });
