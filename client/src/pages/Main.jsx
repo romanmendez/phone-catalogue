f99f2d07eea161054a096fee865d2d55ad8a6ded
Román Méndez <mendezbeck@gmail.com>
2020-03-25 13:08:59 +0100

    styling

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -5,10 +5,7 @@ import { getPhones } from "../redux/actions";
 import DetailCard from "../components/DetailCard";
 import PhoneImage from "../components/PhoneImage";
 import { MainContainer, Row, Column, List } from "../styles/Styles";
-<<<<<<< Updated upstream
 import Loading from "../components/Loading";
-=======
->>>>>>> Stashed changes
 
 const Main = ({ loading, phones, getPhones }) => {
   const [selected, setSelected] = useState(null);
@@ -16,7 +13,6 @@ const Main = ({ loading, phones, getPhones }) => {
   useEffect(() => {
     getPhones();
   }, []);
-<<<<<<< Updated upstream
   if (loading) {
     return <Loading />;
   } else {
@@ -32,20 +28,6 @@ const Main = ({ loading, phones, getPhones }) => {
       </MainContainer>
     );
   }
-=======
-
-  return (
-    <MainContainer>
-      <Row>
-        <Column size="25%">
-          <List>{phones && phones.map((phone, i) => <ListItem key={i} {...{ phone, setSelected }} />)}</List>
-        </Column>
-        <Column size="25%">{selected && <PhoneImage {...{ selected }} />}</Column>
-        <Column size="50%">{selected && <DetailCard {...{ selected }} />}</Column>
-      </Row>
-    </MainContainer>
-  );
->>>>>>> Stashed changes
 };
 
 const mapStateToProps = state => ({ phones: state.phones, loading: state.loading });
